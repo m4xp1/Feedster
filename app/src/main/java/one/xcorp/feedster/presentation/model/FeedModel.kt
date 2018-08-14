2abd186d813fcb40d8ee4b94a610cfb0a081dc50
Dmitriy Makarov <xcorp.one@gmail.com>
2018-08-14 17:55:24 +0300

    Added binding in item_feed layout.

--- app/src/main/java/one/xcorp/feedster/presentation/model/FeedModel.kt
+++ app/src/main/java/one/xcorp/feedster/presentation/model/FeedModel.kt
@@ -1,6 +1,6 @@
 package one.xcorp.feedster.presentation.model
 
-data class FeedModel(val image: String,
+data class FeedModel(val imageUrl: String,
                      val title: String,
                      val description: String,
                      val url: String,
