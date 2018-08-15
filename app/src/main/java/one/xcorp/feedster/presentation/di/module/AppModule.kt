08f3f5f9b63d441902194c5e65863ec22c448539
Dmitriy Makarov <xcorp.one@gmail.com>
2018-08-15 10:14:54 +0300

    Remove binding context.

--- app/src/main/java/one/xcorp/feedster/presentation/di/module/AppModule.kt
+++ app/src/main/java/one/xcorp/feedster/presentation/di/module/AppModule.kt
@@ -1,13 +1,8 @@
 package one.xcorp.feedster.presentation.di.module
 
-import android.content.Context
-import dagger.Binds
 import dagger.Module
-import one.xcorp.feedster.presentation.Application
 
 @Module(includes = [ViewModelModule::class])
 abstract class AppModule {
 
-    @Binds
-    abstract fun bindApplicationContext(application: Application): Context
 }
