b4bf3c294bede0625c031f84e065a8ebbec33740
Dmitriy Makarov <xcorp.one@gmail.com>
2018-08-15 03:47:59 +0300

    Added DIFF_CALLBACK in FeedModel.

--- app/src/main/java/one/xcorp/feedster/presentation/model/FeedModel.kt
+++ app/src/main/java/one/xcorp/feedster/presentation/model/FeedModel.kt
@@ -1,7 +1,23 @@
 package one.xcorp.feedster.presentation.model
 
+import android.support.v7.util.DiffUtil
+
 data class FeedModel(val imageUrl: String,
                      val title: String,
                      val description: String,
                      val url: String,
-                     val users: Int)
+                     val users: Int) {
+
+    companion object {
+
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<FeedModel>() {
+            override fun areItemsTheSame(oldItem: FeedModel, newItem: FeedModel): Boolean {
+                return oldItem.url == newItem.url
+            }
+
+            override fun areContentsTheSame(oldItem: FeedModel, newItem: FeedModel): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
