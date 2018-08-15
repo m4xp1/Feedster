2cca3489c26096f6032c4f2e36374b5103e30310
Dmitriy Makarov <xcorp.one@gmail.com>
2018-08-15 08:34:37 +0300

    Added application context in DI.

--- app/src/main/java/one/xcorp/feedster/presentation/di/module/AppModule.kt
+++ app/src/main/java/one/xcorp/feedster/presentation/di/module/AppModule.kt
@@ -1,12 +1,13 @@
 package one.xcorp.feedster.presentation.di.module
 
 import android.content.Context
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import one.xcorp.feedster.presentation.Application
-import javax.inject.Singleton
 
 @Module(includes = [ViewModelModule::class])
-class AppModule {
+abstract class AppModule {
 
+    @Binds
+    abstract fun bindApplicationContext(application: Application): Context
 }
