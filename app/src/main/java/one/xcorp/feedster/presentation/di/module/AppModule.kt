a2f859fb4dcd1963d56b8da1fdffe14aa0886f3b
Dmitriy Makarov <xcorp.one@gmail.com>
2018-08-15 10:19:05 +0300

    Return context binding.

--- app/src/main/java/one/xcorp/feedster/presentation/di/module/AppModule.kt
+++ app/src/main/java/one/xcorp/feedster/presentation/di/module/AppModule.kt
@@ -1,8 +1,13 @@
 package one.xcorp.feedster.presentation.di.module
 
+import android.content.Context
+import dagger.Binds
 import dagger.Module
+import one.xcorp.feedster.presentation.Application
 
 @Module(includes = [ViewModelModule::class])
 abstract class AppModule {
 
+    @Binds
+    abstract fun bindApplicationContext(application: Application): Context
 }
