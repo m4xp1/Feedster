70639e735878ed40bde330d52d084884da65ffdf
Dmitriy Makarov <xcorp.one@gmail.com>
2018-08-16 00:22:32 +0300

    Added mapping FeedsPageEntity -> FeedsPage.

--- app/src/main/java/one/xcorp/feedster/data/entity/DataMapper.kt
+++ app/src/main/java/one/xcorp/feedster/data/entity/DataMapper.kt
@@ -1,6 +1,9 @@
 package one.xcorp.feedster.data.entity
 
 import one.xcorp.feedster.domain.Feed
+import one.xcorp.feedster.domain.FeedsPage
+
+fun FeedsPageEntity.toFeedsPage() = FeedsPage(this.feeds.toFeed())
 
 fun FeedEntity.toFeed() = Feed(this.imageUrl, this.title,
         this.description, this.url, this.users)
