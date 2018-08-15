4625c2e34e3fbd6e5107b86baea1475ffaac500b
Dmitriy Makarov <xcorp.one@gmail.com>
2018-08-16 01:54:49 +0300

    Added NetworkModule in DI.

--- app/src/main/java/one/xcorp/feedster/presentation/di/module/AppModule.kt
+++ app/src/main/java/one/xcorp/feedster/presentation/di/module/AppModule.kt
@@ -3,11 +3,18 @@ package one.xcorp.feedster.presentation.di.module
 import android.content.Context
 import dagger.Binds
 import dagger.Module
+import one.xcorp.feedster.data.repository.FeedDataRepository
+import one.xcorp.feedster.domain.repository.FeedRepository
 import one.xcorp.feedster.presentation.Application
+import javax.inject.Singleton
 
-@Module(includes = [ViewModelModule::class])
+@Module(includes = [ViewModelModule::class, NetworkModule::class])
 abstract class AppModule {
 
     @Binds
+    @Singleton
     abstract fun bindApplicationContext(application: Application): Context
+
+    @Binds
+    abstract fun bindFeedRepository(repository: FeedDataRepository): FeedRepository
 }
---
+++ app/src/main/java/one/xcorp/feedster/presentation/di/module/NetworkModule.kt
@@ -0,0 +1,25 @@
+package one.xcorp.feedster.presentation.di.module
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor.Level
+import one.xcorp.feedster.R
+import one.xcorp.feedster.data.service.FeedsterService
+import one.xcorp.feedster.data.service.http.HttpClient
+
+@Module
+class NetworkModule {
+
+    @Provides
+    fun provideFeedsterService(context: Context, httpClient: OkHttpClient): FeedsterService {
+        val url = context.getString(R.string.feedster_service_url)
+        return FeedsterService.create(url, httpClient)
+    }
+
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return HttpClient.create(Level.BODY)
+    }
+}
