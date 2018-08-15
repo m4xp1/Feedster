7fc2ff26558991bfe515b4770458305bb1e54c32
Dmitriy Makarov <xcorp.one@gmail.com>
2018-08-15 04:35:53 +0300

    Added feeds LiveData in FeedViewModel.

--- app/src/main/java/one/xcorp/feedster/presentation/viewmodel/FeedViewModel.kt
+++ app/src/main/java/one/xcorp/feedster/presentation/viewmodel/FeedViewModel.kt
@@ -1,8 +1,24 @@
 package one.xcorp.feedster.presentation.viewmodel
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import one.xcorp.feedster.presentation.model.FeedModel
+import one.xcorp.feedster.presentation.model.StatusModel
+import javax.inject.Inject
 
-class FeedViewModel : ViewModel() {
+class FeedViewModel @Inject constructor() : ViewModel() {
 
+    private val _feeds = MutableLiveData<StatusModel<List<FeedModel>>>()
 
+    val feeds: LiveData<StatusModel<List<FeedModel>>>
+        get() = _feeds
+
+    fun load() {
+
+    }
+
+    fun retry() {
+
+    }
 }
